558b4f576080f56a19760a8f97f3d916d4c403e0
pavli <pavli@NOTEBOOK-PAVEL>
2024-01-25 19:00:49 +0100

    m

--- model.py
+++ model.py
@@ -10,41 +10,9 @@ import sys
 import expansion_penalty.expansion_penalty_module as expansion
 import MDS.MDS_module as MDS_module
 
-class STN3d(nn.Module):
-    def __init__(self, num_points = 2500):
-        super(STN3d, self).__init__()
-        self.num_points = num_points
-        self.conv1 = torch.nn.Conv1d(3, 64, 1)
-        self.conv2 = torch.nn.Conv1d(64, 128, 1)
-        self.conv3 = torch.nn.Conv1d(128, 1024, 1)
-        self.fc1 = nn.Linear(1024, 512)
-        self.fc2 = nn.Linear(512, 256)
-        self.fc3 = nn.Linear(256, 9)
-        self.relu = nn.ReLU()
-
-    def forward(self, x):
-        batchsize = x.size()[0]
-        x = F.relu(self.conv1(x))
-        x = F.relu(self.conv2(x))
-        x = F.relu(self.conv3(x))
-        x,_ = torch.max(x, 2)
-        x = x.view(-1, 1024)
-
-        x = F.relu(self.fc1(x))
-        x = F.relu(self.fc2(x))
-        x = self.fc3(x)
-
-        iden = Variable(torch.from_numpy(np.array([1,0,0,0,1,0,0,0,1]).astype(np.float32))).view(1,9).repeat(batchsize,1)
-        if x.is_cuda:
-            iden = iden.cuda()
-        x = x + iden
-        x = x.view(-1, 3, 3)
-        return x
-
 class PointNetfeat(nn.Module):
-    def __init__(self, num_points = 8192, global_feat = True):
+    def __init__(self, num_points = 8192):
         super(PointNetfeat, self).__init__()
-        self.stn = STN3d(num_points = num_points)
         self.conv1 = torch.nn.Conv1d(3, 64, 1)
         self.conv2 = torch.nn.Conv1d(64, 128, 1)
         self.conv3 = torch.nn.Conv1d(128, 1024, 1)
@@ -54,7 +22,6 @@ class PointNetfeat(nn.Module):
         self.bn3 = torch.nn.BatchNorm1d(1024)
 
         self.num_points = num_points
-        self.global_feat = global_feat
     def forward(self, x):
         batchsize = x.size()[0]
         x = F.relu(self.bn1(self.conv1(x)))
