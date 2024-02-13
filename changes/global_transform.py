38e70c670186410aca50dc002f59591aae83e57a
pavli <pavli@NOTEBOOK-PAVEL>
2024-02-13 17:45:52 +0100

    cleaned into files

--- changes/global_transform.py
+++ changes/global_transform.py
@@ -68,12 +68,12 @@ class GlobalAdditiveGeneral(nn.Module):
 
 
 class AdditionalEncoder(nn.Module):
-    def __init__(self, sizes, latents, bottleneck_size=1024):
+    def __init__(self, sizes, latents, bottleneck_size, output_size=1024):
         super().__init__()
         self.bottleneck_size = bottleneck_size
         sizes1 = (3 + 128,) + tuple(sizes[:-1])
         sizes2 = tuple(sizes[-2:]) + (bottleneck_size,)
-        self.transform_extractor = BatchNormConv1DNoAct(bottleneck_size, bottleneck_size)
+        self.transform_extractor = BatchNormConv1DNoAct(bottleneck_size, output_size)
         self.convs1 = nn.Sequential(*make(sizes1, lambda x, y: nn.Sequential(BatchNormConv1D(x, y))))
         self.convs2 = nn.Sequential(*make(sizes2, lambda x, y: nn.Sequential(BatchNormConv1D(x, y))))
         self.gt = GlobalTransformDepthSep(sizes[-2], bottleneck_size, (3 + 128,), latents)
